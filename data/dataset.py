2ae4ea5d9efdab741f9bb4661f7bbc465e7671ef
Kang-Min-Ku <netisen4@gmail.com>
2023-11-29 18:42:52 +0900

    data loader

---
+++ data/dataset.py
@@ -0,0 +1,189 @@
+# from https://pytorch.org/vision/0.11/_modules/torchvision/datasets/folder.html
+################################################################################
+################################################################################
+# copied from folder.py
+
+from typing import Any, Callable, cast, Dict, List, Optional, Tuple
+from typing import Union
+from PIL import Image
+from torchvision.datasets import VisionDataset, DatasetFolder
+import os
+
+def has_file_allowed_extension(filename: str, extensions: Tuple[str, ...]) -> bool:
+    """Checks if a file is an allowed extension.
+
+    Args:
+        filename (string): path to a file
+        extensions (tuple of strings): extensions to consider (lowercase)
+
+    Returns:
+        bool: True if the filename ends with one of given extensions
+    """
+    return filename.lower().endswith(extensions)
+
+IMG_EXTENSIONS = ('.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm', '.tif', '.tiff', '.webp')
+
+
+def pil_loader(path: str) -> Image.Image:
+    # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
+    with open(path, 'rb') as f:
+        img = Image.open(f)
+        return img.convert('RGB')
+
+
+# TODO: specify the return type
+def accimage_loader(path: str) -> Any:
+    import accimage
+    try:
+        return accimage.Image(path)
+    except IOError:
+        # Potentially a decoding problem, fall back to PIL.Image
+        return pil_loader(path)
+
+
+def default_loader(path: str) -> Any:
+    from torchvision import get_image_backend
+    if get_image_backend() == 'accimage':
+        return accimage_loader(path)
+    else:
+        return pil_loader(path)
+
+def make_dataset(
+    directory: str,
+    class_to_idx: Optional[Dict[str, int]] = None,
+    extensions: Optional[Tuple[str, ...]] = None,
+    is_valid_file: Optional[Callable[[str], bool]] = None,
+) -> List[Tuple[str, int]]:
+    """Generates a list of samples of a form (path_to_sample, class).
+
+    See :class:`DatasetFolder` for details.
+
+    Note: The class_to_idx parameter is here optional and will use the logic of the ``find_classes`` function
+    by default.
+    """
+    directory = os.path.expanduser(directory)
+
+    if class_to_idx is None:
+        _, class_to_idx = find_classes(directory)
+    elif not class_to_idx:
+        raise ValueError("'class_to_index' must have at least one entry to collect any samples.")
+
+    both_none = extensions is None and is_valid_file is None
+    both_something = extensions is not None and is_valid_file is not None
+    if both_none or both_something:
+        raise ValueError("Both extensions and is_valid_file cannot be None or not None at the same time")
+
+    if extensions is not None:
+
+        def is_valid_file(x: str) -> bool:
+            return has_file_allowed_extension(x, cast(Tuple[str, ...], extensions))
+
+    is_valid_file = cast(Callable[[str], bool], is_valid_file)
+
+    instances = []
+    available_classes = set()
+    for target_class in sorted(class_to_idx.keys()):
+        class_index = class_to_idx[target_class]
+        target_dir = os.path.join(directory, target_class)
+        if not os.path.isdir(target_dir):
+            continue
+        for root, _, fnames in sorted(os.walk(target_dir, followlinks=True)):
+            for fname in sorted(fnames):
+                if is_valid_file(fname):
+                    path = os.path.join(root, fname)
+                    item = path, class_index
+                    instances.append(item)
+
+                    if target_class not in available_classes:
+                        available_classes.add(target_class)
+
+    empty_classes = set(class_to_idx.keys()) - available_classes
+    if empty_classes:
+        msg = f"Found no valid file for the classes {', '.join(sorted(empty_classes))}. "
+        if extensions is not None:
+            msg += f"Supported extensions are: {', '.join(extensions)}"
+        raise FileNotFoundError(msg)
+
+    return instances
+
+class CustomDatasetFolder(VisionDataset):
+    def __init__(
+        self,
+        root: str,
+        loader: Callable[[str], Any],
+        class_list: List[str],
+        extensions: Optional[Tuple[str, ...]] = None,
+        transform: Optional[Callable] = None,
+        target_transform: Optional[Callable] = None,
+        is_valid_file: Optional[Callable[[str], bool]] = None,
+        custom_classes: Optional[Dict[str, int]] = None,
+    ) -> None:
+        super().__init__(root, transform=transform, target_transform=target_transform)
+        classes, class_to_idx = self.find_classes(class_list)
+        samples = self.make_dataset(self.root, class_to_idx, extensions, is_valid_file)
+
+        self.custom_classes = custom_classes
+        self.loader = loader
+        self.extensions = extensions
+
+        if self.custom_classes is None:
+            self.classes = classes
+            self.class_to_idx = class_to_idx
+        else:
+            self.classes = self.custom_classes
+            self.class_to_idx = {label : idx for idx, label in enumerate(self.custom_classes)}
+        self.samples = samples
+        self.targets = [s[1] for s in samples]
+
+    @staticmethod
+    def make_dataset(
+        directory: str,
+        class_to_idx: Dict[str, int],
+        extensions: Optional[Tuple[str, ...]] = None,
+        is_valid_file: Optional[Callable[[str], bool]] = None,
+    ) -> List[Tuple[str, int]]:
+
+        if class_to_idx is None:
+            raise ValueError("The class_to_idx parameter cannot be None.")
+        return make_dataset(directory, class_to_idx, extensions=extensions, is_valid_file=is_valid_file)
+
+    def find_classes(self, class_list: List[str]) -> Tuple[List[str], Dict[str, int]]:
+        return class_list, {label : idx for idx, label in enumerate(class_list)}
+
+    def __getitem__(self, index: int) -> Tuple[Any, Any]:
+
+        path, target = self.samples[index]
+        sample = self.loader(path)
+        if self.transform is not None:
+            sample = self.transform(sample)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return sample, target
+
+    def __len__(self) -> int:
+        return len(self.samples)
+
+
+class CustomImageFolder(CustomDatasetFolder):
+    def __init__(
+        self,
+        root: str,
+        class_list: List[str],
+        transform: Optional[Callable] = None,
+        target_transform: Optional[Callable] = None,
+        loader: Callable[[str], Any] = default_loader,
+        is_valid_file: Optional[Callable[[str], bool]] = None,
+        custom_classes: Optional[Dict[str, int]] = None,
+    ):
+        super().__init__(
+        root,
+        loader,
+        class_list,
+        IMG_EXTENSIONS if is_valid_file is None else None,
+        transform=transform,
+        target_transform=target_transform,
+        is_valid_file=is_valid_file,
+        custom_classes=custom_classes,
+    )
+        self.imgs = self.samples
---
+++ data/loader.py
@@ -0,0 +1,115 @@
+import torch
+from torch.utils.data import DataLoader
+import torchvision
+import torchvision.transforms as transforms
+
+from .dataset import CustomImageFolder
+
+import numpy as np
+import argparse
+import re
+import os
+
+class Loader:
+    def __init__(self, root:str, args:argparse.Namespace):
+        self.root = root
+        self.args = args
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, ), (0.5, ))
+        ])
+        np.random.seed(self.args.seed)
+
+    def load(self, dataset, dataset_path=None, num_deleted_class=0):
+        """
+        dataset should be "cifar100" or "dog"
+        dataset_path should be specifed if dataset is "dog"
+        """
+        assert dataset in ["cifar100", "dog"], "dataset must be cifar100 or dog"
+
+        if dataset == "cifar100":
+            return self._load_cifar100(dataset_path, num_deleted_class)
+        elif dataset == "dog":
+            return self._load_dog(dataset_path, num_deleted_class)
+        
+    def _load_cifar100(self, dataset_path, num_deleted_class):
+        """
+        50000 training samples and 10000 test samples
+        500 samples for each class
+        """
+        trainset = torchvision.datasets.CIFAR100(root=self.root,
+                                                 train=True,
+                                                download=True,
+                                                transform=self.transform)
+        testset = torchvision.datasets.CIFAR100(root=self.root,
+                                                train=False,
+                                                download=True,
+                                                transform=self.transform)
+        
+        if num_deleted_class > 0:
+            dataset_class = trainset.classes
+            num_class = len(dataset_class)
+            num_remain_class = num_class - num_deleted_class
+
+            assert num_remain_class > 0, "num_remain_class must be positive"
+
+            remain_class = np.random.choice(dataset_class, num_remain_class, replace=False)
+            
+            train_class_indices = [trainset.class_to_idx[cls] for cls in remain_class]
+            train_filtered_indices = [i for i, (_, label) in enumerate(trainset) if label in train_class_indices]
+            trainset = torch.utils.data.Subset(trainset, train_filtered_indices)
+
+            test_class_indices = [testset.class_to_idx[cls] for cls in remain_class]
+            test_filtered_indices = [i for i, (_, label) in enumerate(testset) if label in test_class_indices]
+            testset = torch.utils.data.Subset(testset, test_filtered_indices)
+            
+        trainloader = DataLoader(trainset, batch_size=self.args.batch_size, shuffle=True, num_workers=self.args.num_workers)
+        testloader = DataLoader(testset, batch_size=self.args.batch_size, shuffle=False, num_workers=self.args.num_workers)
+
+        return trainloader, testloader
+    
+    def _load_dog(self, dataset_path, num_deleted_class, image_path="Images"):
+        """
+        20580 images
+        Assume the situation that "images.tar" file provided by the Standford Dogs has been decomposed
+        """
+        assert dataset_path is not None, "dataset_path must be specified"
+
+        image_dirs = os.listdir(os.path.join(self.root, dataset_path, image_path))
+        pattern = re.compile(r"-([a-zA-Z_]+)$")
+        dataset_class = [re.findall(pattern, d)[0] for d in image_dirs]
+
+        dataset = CustomImageFolder(os.path.join(self.root, dataset_path, image_path),
+                                    transform=self.transform,
+                                    class_list=image_dirs,
+                                    custom_classes=dataset_class)
+        
+        if num_deleted_class > 0:
+            num_class = len(dataset_class)
+            num_remain_class = num_class - num_deleted_class
+
+            assert num_remain_class > 0, "num_remain_class must be positive"
+
+            remain_class = np.random.choice(dataset_class, num_remain_class, replace=False)
+
+            class_indices = [dataset.class_to_idx[cls] for cls in remain_class]
+            filtered_indices = [i for i, (_, label) in enumerate(dataset) if label in class_indices]
+            dataset = torch.utils.data.Subset(dataset, filtered_indices)
+
+        train_ratio, _, test_ratio = self.args.dataset_split
+        if train_ratio + test_ratio < 1.0:
+            margin = 1.0 - train_ratio - test_ratio
+            train_ratio += margin
+        trainset, testset = torch.utils.data.random_split(dataset, [train_ratio, test_ratio])
+
+        trainloader = DataLoader(trainset, batch_size=self.args.batch_size, shuffle=True, num_workers=self.args.num_workers)
+        testloader = DataLoader(testset, batch_size=self.args.batch_size, shuffle=False, num_workers=self.args.num_workers)
+
+        return trainloader, testloader
+
+
+
+
+        
+
+
