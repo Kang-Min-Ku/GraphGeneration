c6e944a53df22e458151605d00f085a14e832aab
Kang-Min-Ku <netisen4@gmail.com>
2023-11-30 20:57:21 +0900

    feature extractor

---
+++ utils/extractor.py
@@ -0,0 +1,38 @@
+import torch
+import os
+
+def extract_feature(model,
+                    train_loader, test_loader,
+                    is_save=True, save_path="save", model_name="resnet", dataset_name="cifar"):
+    model.eval()
+    device = next(model.parameters()).device
+    embedding = []
+    label = []
+
+    with torch.no_grad():
+        for images, labels in train_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+
+            # Forward pass
+            outputs = model(images, is_extract=True)
+            embedding.append(outputs)
+            label.append(labels)
+
+        for images, labels in test_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+
+            # Forward pass
+            outputs = model(images, is_extract=True)
+            embedding.append(outputs)
+            label.append(labels)
+
+    embedding = torch.cat(embedding, dim=0)
+    label = torch.cat(label, dim=0)
+    _, embedding_size = embedding.shape
+
+    if is_save:
+        torch.save([embedding, label], os.path.join(save_path, f"{dataset_name}_{model_name}_emb_{embedding_size}.pt"))
+
+    return embedding, label
